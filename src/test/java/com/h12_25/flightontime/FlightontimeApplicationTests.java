c387b7975cfb229b914fb181214037912b4d1fe6
Nosaka King <raulduran2808@gmail.com>
2025-12-22 10:20:26 -0500

    feat: spring initializer

---
+++ src/test/java/com/h12_25/flightontime/FlightontimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h12_25.flightontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightontimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
